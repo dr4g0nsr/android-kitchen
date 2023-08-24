db16a65e60af4727ef142a8f7482e55ccce3f491
Dragutin Cirkovic <dragonmen@gmail.com>
2023-08-24 17:04:03 +0200

    Update unpack_vendor.sh

--- scripts/unpack/unpack_vendor.sh
+++ scripts/unpack/unpack_vendor.sh
@@ -2,29 +2,30 @@
 
 clear
 
+# Source variables from the "dumpvars.sh" script located in the "scripts" directory
 source scripts/dumpvars.sh
 
-echo "vendor.dat.br/dat/img解包"
+echo "Unpacking vendor.dat.br/dat/img"
 
-read -p "请将需要解包的vendor.dat.br/dat/img放入工具根目录，并按下回车" var
+read -p "Please place the vendor.dat.br/dat/img file you want to unpack in the tool's root directory and press Enter" var
 
 if [ -e "$LOCALDIR/vendor.new.dat.br" ]; then
-	echo "检测到vendor.new.dat.br"
+	echo "Detected vendor.new.dat.br"
 	"$bin/bin/brotli" -d "$LOCALDIR/vendor.new.dat.br"
 	python "$bin/sdat2img.py" vendor.transfer.list vendor.new.dat vendor.img
 	bash $unpack/unpackimg.sh vendor
- 	read -p "解包完成" var
+ 	read -p "Unpacking completed" var
 elif [ -e "$LOCALDIR/vendor.new.dat" ]; then
-	echo "检测到vendor.new.dat"
+	echo "Detected vendor.new.dat"
     python "$bin/sdat2img.py" vendor.transfer.list vendor.new.dat vendor.img
     bash $unpack/unpackimg.sh vendor
- 	read -p "解包完成" var
+ 	read -p "Unpacking completed" var
 elif [ -e "$LOCALDIR/vendor.img" ]; then
-	echo "检测到vendor.img"
+	echo "Detected vendor.img"
     bash $unpack/unpackimg.sh vendor
- 	read -p "解包完成" var
+ 	read -p "Unpacking completed" var
 else
-	read -p "没有检测到任何需要解包的文件" var
+	read -p "No files detected to unpack" var
 fi
 
 bash main.sh
