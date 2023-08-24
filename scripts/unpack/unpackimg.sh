e6d116aeee3a7653ab6b99647029ce55e91c1ad7
Dragutin Cirkovic <dragonmen@gmail.com>
2023-08-24 17:04:31 +0200

    Update unpackimg.sh

--- scripts/unpack/unpackimg.sh
+++ scripts/unpack/unpackimg.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# Source variables from the "dumpvars.sh" script located in the "scripts" directory
 source scripts/dumpvars.sh
 
+# Change directory to the tool's root directory
 cd $LOCALDIR
 
+# Get the argument passed to the script (assuming it's a species identifier)
 species="$1"
+
+# Create a directory with the species identifier
 mkdir ./"$species"
 
-echo "正在解包$species.img......."
+echo "Unpacking $species.img......."
+# Run the imgextractor.py script with sudo to extract the specified image
 sudo python3 $bin/imgextractor/imgextractor.py ./$species'.img' ./$species
+
+# Remove the original species directory
 rm -rf /"$species"
