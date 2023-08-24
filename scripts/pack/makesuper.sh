62e510006b57499adee46e1691449eb5422c20ee
Dragutin Cirkovic <dragonmen@gmail.com>
2023-08-24 17:00:35 +0200

    Update makesuper.sh

--- scripts/pack/makesuper.sh
+++ scripts/pack/makesuper.sh
@@ -1,68 +1,68 @@
 #!/bin/bash
 
+# Source variables from the "dumpvars.sh" script located in the parent directory
 source ../dumpvars.sh
-
 
 echo "
 
-开始打包
+Start Packaging
 
-请确保要打包的所有img在工具根目录下且为rimg (必须遵守)
+Make sure all the img files you want to package are in the tool's root directory and named as rimg (must follow this convention).
 
-当前支持打包super.img的分区有 system vendor product odm
+Currently supported partitions for super.img packaging: system vendor product odm
 
-super分区大小为要打包的rimg的总大小+1G(至少要+1G否则可能会报错)
+The size of the super partition should be the total size of the rimg files you want to package + 1G (at least +1G to avoid potential errors).
 
-super最终实际可用大小等于要打包的rimg的总大小
+The actual usable size of the super partition will be equal to the total size of the rimg files you want to package.
 
-打包数据用G为单位时候要为整数
+When packaging data using G as the unit, it should be a whole number.
 
-如果打包数据不为整数时用M为单位
+If the packaging data is not a whole number, use M as the unit.
 
-用B为单位打包时无需带单位
+When using B as the unit, you don't need to include the unit.
 "
 
-if [ -e ./system.img ];then
+if [ -e ./system.img ]; then
  mv ./system.img ./bin/build_super/
 fi
  
-if [ -e ./vendor.img ];then
+if [ -e ./vendor.img ]; then
  mv ./vendor.img ./bin/build_super/
 fi
 
-if [ -e ./product.img ];then
+if [ -e ./product.img ]; then
  mv ./product.img ./bin/build_super/
 fi
 
-if [ -e ./odm.img ];then
+if [ -e ./odm.img ]; then
  mv ./odm.img ./bin/build_super/
 fi
 
 cd ./bin/build_super
-cat ./misc_into.txt >>./build_super.txt
+cat ./misc_into.txt >> ./build_super.txt
 
-read -p "请输入你要打包的分区 (多个分区记得留空格): " partition
-read -p "请输入super分区大小: " supersize
+read -p "Please enter the partitions you want to package (separated by spaces): " partition
+read -p "Please enter the size of the super partition: " supersize
 
 superM="$(echo "$supersize" | sed 's/M//g')"
 superG="$(echo "$supersize" | sed 's/G//g')"
 
-if [ $(echo "$supersize" | grep 'M') ];then
+if [ $(echo "$supersize" | grep 'M') ]; then
  superssize="$(($superM*1024*1024))"
-elif [ $(echo "$supersize" | grep 'G') ];then
+elif [ $(echo "$supersize" | grep 'G') ]; then
  superssize="$(($superG*1024*1024*1024))"
 else
  superssize="$supersize"
 fi
 
-read -p "请输入super最终实际可用大小: " size
+read -p "Please enter the actual usable size of the super partition: " size
 
 sizeM="$(echo "$size" | sed 's/M//g')"
 sizeG="$(echo "$size" | sed 's/G//g')"
 
-if [ $(echo "$size" | grep 'M') ];then
+if [ $(echo "$size" | grep 'M') ]; then
  ssize="$(($sizeM*1024*1024))"
-elif [ $(echo "$size" | grep 'G') ];then
+elif [ $(echo "$size" | grep 'G') ]; then
  ssize="$(($sizeG*1024*1024*1024))"
 else
  ssize="$size"
@@ -73,10 +73,10 @@ super_main_partition_list=$partition
 super_super_device_size=$superssize
 super_main_group_size=$ssize
 " >> ./build_super.txt
-echo "super.img生成信息整合完毕,正在生成super.img....."
+echo "Super.img generation information integrated, generating super.img..."
 python3 ./build_super_image.py ./build_super.txt ./super.img
 rm -rf ./build_super.txt
-echo "super.img已生成，已输出至super目录"
+echo "super.img has been generated and output to the super directory"
 cd ../../
 rm -rf ./super
 mkdir ./super
