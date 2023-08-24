cf54b7a5414c5725b23a69145f7f60294195e7f9
Dragutin Cirkovic <dragonmen@gmail.com>
2023-08-24 16:59:41 +0200

    Update makeimg.sh

--- scripts/pack/makeimg.sh
+++ scripts/pack/makeimg.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+# Source variables from the "dumpvars.sh" script located in the parent directory
 source ../dumpvars.sh
 
+# Get the current directory and change to it
 LOCALDIR=`cd "$( dirname $0 )" && pwd`
 cd $LOCALDIR
 
 echo ""
-read -p "请输入要打包的分区(别带.img): " species
+read -p "Please enter the name of the partition to package (without .img extension): " species
 
 echo "
-开始打包
+Start packaging
 
-当前img大小为: 
+Current image size:
 
 _________________
 
@@ -22,51 +24,51 @@ _________________
 `du -sb ./out/$species | awk '{print $1}' | sed 's/$/&B/'`
 _________________
 
-使用G为单位打包时必须带单位且为整数
-使用B为单位打包时无需带单位且在自动识别的大小添加一定大小
-推荐用M为单位大小进行打包需带单位且在自动识别的大小添加至少130M大小
+When packaging using G as unit, you must provide the unit and it should be an integer.
+When packaging using B as unit, you don't need to provide the unit, and a certain size will be added to the automatically detected size.
+It's recommended to use M as unit for packaging. You need to provide the unit and add at least 130M to the automatically detected size.
 "
 
-read -p "请输入要打包的数值: " size
+read -p "Please enter the value for packaging: " size
 
 M="$(echo "$size" | sed 's/M//g')"
 G="$(echo "$size" | sed 's/G//g')"
 
-if [ $(echo "$size" | grep 'M') ];then
+if [ $(echo "$size" | grep 'M') ]; then
  ssize=$(($M*1024*1024))
-elif [ $(echo "$size" | grep 'G') ];then
+elif [ $(echo "$size" | grep 'G') ]; then
  ssize=$(($G*1024*1024*1024))
 else
  ssize=$size
 fi
 
-if [ $species = "system" ];then
+if [ $species = "system" ]; then
  $bin/mkuserimg_mke2fs.sh ./out/$species/$species ./out/$species'.img' ext4 /$species $ssize -j '0' -T '1230768000' -C ./out/config/$species'_fs_config' -L $species ./out/config/$species'_file_contexts' 2> ./out/error.log
 else
  $bin/mkuserimg_mke2fs.sh ./out/$species/ ./out/$species'.img' ext4 /$species $ssize -j '0' -T '1230768000' -C ./out/config/$species'_fs_config' -L $species ./out/config/$species'_file_contexts' 2> ./out/error.log 
 fi
 sed -i '1d' ./out/error.log
 
-if [ -s ./out/error.log ];then
- echo "打包失败，请去out目录检查错误日志"
+if [ -s ./out/error.log ]; then
+ echo "Packaging failed, please check the error log in the 'out' directory"
 else
- echo "打包完成"
+ echo "Packaging complete"
  rm -rf ./out/error.log
- echo "输出至SGSI文件夹"
+ echo "Output to the SGSI folder"
 fi
 
-if [ -e ./SGSI ];then
+if [ -e ./SGSI ]; then
  rm -rf ./SGSI
  mkdir ./SGSI
 else
  mkdir ./SGSI
 fi
 
-if [ -e ./SGSI ];then
+if [ -e ./SGSI ]; then
  mv ./out/$species'.img' ./SGSI
  cp -r ./刷机教程.txt ./SGSI
- #检测精简app的zip
- if [ -e ./out/delete.zip ];then
+ # Check for the zip containing deleted apps
+ if [ -e ./out/delete.zip ]; then
    mv ./out/delete.zip ./SGSI
  fi
 fi
