4b892acd121eaadec8132c3fecb7e852b8bdc474
Dragutin Cirkovic <dragonmen@gmail.com>
2023-08-24 17:03:30 +0200

    Update unpack_system.sh

--- scripts/unpack/unpack_system.sh
+++ scripts/unpack/unpack_system.sh
@@ -2,29 +2,30 @@
 
 clear
 
+# Source variables from the "dumpvars.sh" script located in the "scripts" directory
 source scripts/dumpvars.sh
 
-echo "system.dat.br/dat/img解包"
+echo "Unpacking system.dat.br/dat/img"
 
-read -p "请将需要解包的system.dat.br/dat/img放入工具根目录，并按下回车" var
+read -p "Please place the system.dat.br/dat/img file you want to unpack in the tool's root directory and press Enter" var
 
 if [ -e "$LOCALDIR/system.new.dat.br" ]; then
-	echo "检测到system.new.dat.br"
+	echo "Detected system.new.dat.br"
 	"$bin/bin/brotli" -d "$LOCALDIR/system.new.dat.br"
 	python "$bin/sdat2img.py" system.transfer.list system.new.dat system.img
 	bash $unpack/unpackimg.sh system
-	read -p "解包完成" var
+	read -p "Unpacking completed" var
 elif [ -e "$LOCALDIR/system.new.dat" ]; then
-	echo "检测到system.new.dat"
+	echo "Detected system.new.dat"
     python "$bin/sdat2img.py" system.transfer.list system.new.dat system.img
     bash $unpack/unpackimg.sh system
-  	read -p "解包完成" var
+  	read -p "Unpacking completed" var
 elif [ -e "$LOCALDIR/system.img" ]; then
-	echo "检测到system.img"
+	echo "Detected system.img"
     bash $unpack/unpackimg.sh system
- 	read -p "解包完成" var
+ 	read -p "Unpacking completed" var
 else
-	read -p "没有检测到任何需要解包的文件" var
+	read -p "No files detected to unpack" var
 fi
 
 bash main.sh
